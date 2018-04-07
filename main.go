65481fc77a47dfe3c201627c8413d2ee8ea038d5
Artem Vitiuk <artem@vitiuk.me>
2018-04-07 22:39:37 +0200

    Add MHZ19 support

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/eclipse/paho.mqtt.golang"
 	"github.com/go-ble/ble"
 	"github.com/go-ble/ble/linux"
+	"github.com/jacobsa/go-serial/serial"
 	"github.com/spf13/viper"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
@@ -24,18 +25,27 @@ type config struct {
 		Preffix string
 	}
 	Bme280 struct {
+		Enabled  bool
 		Interval time.Duration
 		Address  int
 	}
 	Ble struct {
+		Enabled      bool
 		Interval     time.Duration
 		Duration     time.Duration
 		MqttPreffix  string   `mapstructure:"mqtt_preffix"`
 		KnownDevices []string `mapstructure:"known_devices"`
 	}
 	Pir struct {
+		Enabled    bool
 		Pin        string
 		MqttSuffix string `mapstructure:"mqtt_suffix"`
+	}
+	Mhz19 struct {
+		Enabled    bool
+		Interval   time.Duration
+		MqttSuffix string `mapstructure:"mqtt_suffix"`
+		Port       string
 	}
 }
 
@@ -85,6 +95,40 @@ func initMqtt() mqtt.Client {
 	return c
 }
 
+func readCO2() int {
+	co2Req := []byte{0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}
+	s, err := serial.Open(serial.OpenOptions{
+		PortName:        cfg.Mhz19.Port,
+		BaudRate:        9600,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 9,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+
+	n, err := s.Write(co2Req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 10)
+	if n, err = s.Read(buf); err == nil && n == 9 {
+		var sum int
+		for i := 1; i < 8; i++ {
+			sum += int(buf[i])
+		}
+
+		if ((sum%256)^0xFF)+1 == int(buf[8]) {
+			return int(buf[2])*256 + int(buf[3])
+		}
+	}
+
+	return -1
+}
+
 func x(suffix string) string {
 	return cfg.Mqtt.Preffix + suffix
 }
@@ -92,14 +136,26 @@ func x(suffix string) string {
 func main() {
 	flag.Parse()
 
-	bleDevice, err := linux.NewDevice()
-	if err != nil {
-		log.Fatalf("Error initializing of a ble device : %s", err)
+	//TODO compiler props?
+	var bleDevice *linux.Device
+	if cfg.Ble.Enabled {
+		var err error
+		if bleDevice, err = linux.NewDevice(); err != nil {
+			log.Fatalf("Error initializing of a ble device : %s", err)
+		}
 	}
 
 	rpiAdaptor := raspi.NewAdaptor()
-	bme280 := i2c.NewBME280Driver(rpiAdaptor, i2c.WithAddress(cfg.Bme280.Address)) // 0x76
-	pir := gpio.NewPIRMotionDriver(rpiAdaptor, cfg.Pir.Pin)
+
+	var bme280 *i2c.BME280Driver
+	if cfg.Bme280.Enabled {
+		bme280 = i2c.NewBME280Driver(rpiAdaptor, i2c.WithAddress(cfg.Bme280.Address))
+	}
+
+	var pir *gpio.PIRMotionDriver
+	if cfg.Pir.Enabled {
+		pir = gpio.NewPIRMotionDriver(rpiAdaptor, cfg.Pir.Pin)
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -113,41 +169,62 @@ func main() {
 	}
 
 	work := func() {
-		gobot.Every(cfg.Ble.Interval, func() {
-			ctx, cancel := context.WithTimeout(ctx, cfg.Ble.Duration)
-			defer cancel()
-			bleDevice.Scan(ctx, false, func(a ble.Advertisement) {
-				for _, item := range cfg.Ble.KnownDevices {
-					if item == a.Addr().String() {
-						mqttClient.Publish(cfg.Ble.MqttPreffix+item, 1, false, []byte("home"))
+		if cfg.Ble.Enabled {
+			gobot.Every(cfg.Ble.Interval, func() {
+				ctx, cancel := context.WithTimeout(ctx, cfg.Ble.Duration)
+				defer cancel()
+				bleDevice.Scan(ctx, false, func(a ble.Advertisement) {
+					for _, item := range cfg.Ble.KnownDevices {
+						if item == a.Addr().String() {
+							mqttClient.Publish(cfg.Ble.MqttPreffix+item, 1, false, []byte("home"))
+						}
 					}
+				})
+			})
+		}
+
+		if cfg.Pir.Enabled {
+			pir.On(gpio.MotionDetected, onMotionHandler)
+			pir.On(gpio.MotionStopped, onMotionHandler)
+		}
+
+		if cfg.Bme280.Enabled {
+			gobot.Every(cfg.Bme280.Interval, func() {
+				if t, err := bme280.Temperature(); err == nil {
+					mqttClient.Publish(x("temperature"), 1, false, float32bytes(t))
+				}
+
+				if p, err := bme280.Pressure(); err == nil {
+					mqttClient.Publish(x("pressure"), 1, false, float32bytes(p/100))
+				}
+
+				if h, err := bme280.Humidity(); err == nil {
+					mqttClient.Publish(x("humidity"), 1, false, float32bytes(h))
 				}
 			})
-		})
+		}
 
-		pir.On(gpio.MotionDetected, onMotionHandler)
-		pir.On(gpio.MotionStopped, onMotionHandler)
-
-		gobot.Every(cfg.Bme280.Interval, func() {
-			if t, err := bme280.Temperature(); err == nil {
-				mqttClient.Publish(x("temperature"), 1, false, float32bytes(t))
-			}
-
-			if p, err := bme280.Pressure(); err == nil {
-				mqttClient.Publish(x("pressure"), 1, false, float32bytes(p/100))
-			}
-
-			if h, err := bme280.Humidity(); err == nil {
-				mqttClient.Publish(x("humidity"), 1, false, float32bytes(h))
-			}
-		})
+		if cfg.Mhz19.Enabled {
+			gobot.Every(cfg.Mhz19.Interval, func() {
+				if co2 := readCO2(); co2 != -1 {
+					mqttClient.Publish(x(cfg.Mhz19.MqttSuffix), 1, false, co2)
+				}
+			})
+		}
 
 		// mqttAdaptor.On("hello", func(msg mqtt.Message) { fmt.Println(msg) })
 	}
 
+	var devices []gobot.Device
+	if cfg.Bme280.Enabled {
+		devices = append(devices, bme280)
+	}
+	if cfg.Pir.Enabled {
+		devices = append(devices, pir)
+	}
 	robot := gobot.NewRobot("bot",
 		[]gobot.Connection{rpiAdaptor},
-		[]gobot.Device{bme280, pir},
+		devices,
 		work,
 	)
 
